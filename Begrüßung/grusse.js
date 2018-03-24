a9a65547fc7160a86c2ad2469faea24ebe8777b3
Kekser <Kekser@192.168.0.39>
2018-03-24 12:06:05 +0100

    Erster Upload + Begrüßung

---
+++ Begrüßung/grusse.js
@@ -0,0 +1,9 @@
+//dklfsjdal�53355553555f555
+var person = prompt(name);
+// document.getElementById("nameUser").innerHTML =
+document.write("Hello " + person + " !How are you today?");
+// function main(): void { }
+// document.addEventListener('DOMContentLoaded', main);
+// <button onlick="myFunction()">Klick mich</button>
+//<p id="nameUser"></p> 
+//# sourceMappingURL=grusse.js.map
---
+++ Begrüßung/grusse.ts
@@ -0,0 +1,13 @@
+
+//dklfsjdalö53355553555f555
+    var person: String = prompt(name);
+   
+       // document.getElementById("nameUser").innerHTML =
+    document.write("Hello " + person + " !How are you today?");
+   
+
+      // function main(): void { }
+      // document.addEventListener('DOMContentLoaded', main);
+
+// <button onlick="myFunction()">Klick mich</button>
+//<p id="nameUser"></p>
